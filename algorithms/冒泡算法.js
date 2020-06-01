1a82f1cf6db5efda02e7e3c35b95f1d3a3f10d32
menyongkang <1075162046@qq.com>
2020-06-01 23:36:35 +0800

    null

---
+++ algorithms/冒泡算法.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+  console.log('i---',i);
+  for (var i = 1, len = arr.length; i < len - 1; ++i) {
+    console.log('i---',i);
+    console.log('len---',len);
+   
+    for (var j = 0; j <=len - i; ++j) {
+      console.log('j----',j);
+      console.log('arr[j]----',arr[j]);
+      console.log('arr[j+1]----',arr[j+1]);
+      console.log(arr[j] > arr[j + 1])
+      
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+      console.log('arr----',arr);
+
+    }
+    console.log('arr----',arr);
+  }
+}
+
+bubbleSort([9,4,1,0]);
+
+// 外部循环结束条件 length-1次，外部的递增 值++i,有限判断结束条件
+// 内部循环结束条件 小于等于length-i，递增值为++j,这样确保最后一位元素的大小q
+
+
