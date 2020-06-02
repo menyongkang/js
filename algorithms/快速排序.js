ffa4f97aece9239fb75cfb0b48207e9e737c2b5b
menyongkang <1075162046@qq.com>
2020-06-02 23:22:30 +0800

    格式化

--- algorithms/快速排序.js
+++ algorithms/快速排序.js
@@ -1,16 +1,18 @@
-function qSort(arr){
-  var left=[],right=[],len=arr.length;
-  if(len<=1) return arr;
-  var base=arr[0];
-  for(var i=1;i<len;i++){
-    if(arr[i]<=base){
+function qSort(arr) {
+  var left = [],
+    right = [],
+    len = arr.length;
+  if (len <= 1) return arr;
+  var base = arr[0];
+  for (var i = 1; i < len; i++) {
+    if (arr[i] <= base) {
       left.push(arr[i]);
-    }else{
+    } else {
       right.push(arr[i])
     }
   }
-  return [...qSort(left),...[base],...qSort(right)];
+  return [...qSort(left), ...[base], ...qSort(right)];
 
 }
 
-console.log(qSort([3,0,4,5,2,-1]))
+console.log(qSort([3, 0, 4, 5, 2, -1]))
